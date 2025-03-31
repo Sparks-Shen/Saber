715850fdf721821313f79aed8dd7cde46b1e5c33
Sparks-Shen <2589755363@qq.com>
2025-03-31 22:59:49 +0800

    Add files via upload

---
+++ Snake_game/Food.py
@@ -0,0 +1,20 @@
+import random
+
+class Food:
+    def __init__(self, length, width, body):
+        while(True):
+            x = random.randint(1, width-2)
+            y = random.randint(1, length-2)
+            food_pos = [x, y]
+            if food_pos not in body:
+                self.pos = food_pos
+                break
+
+    def update(self, length, width, body):
+        while(True):
+            x = random.randint(1, width-2)
+            y = random.randint(1, length-2)
+            food_pos = [x, y]
+            if food_pos not in body:
+                self.pos = food_pos
+                break      
